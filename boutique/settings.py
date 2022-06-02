3d6a7c1842253a528cf165e9322e2b6d2575a159
roomacarthur <roomacarthur@gmail.com>
2022-06-02 18:49:39 +0100

    settup allauth

--- boutique/settings.py
+++ boutique/settings.py
@@ -83,6 +83,16 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/' #temp url of /success for testing all auth is working
+
 WSGI_APPLICATION = 'boutique.wsgi.application'
 
 
